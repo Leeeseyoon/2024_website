3eb2af2bd183a78f88196718a9b2853c55d0b551
cxexxe <err19@naver.com>
2024-11-06 21:47:56 +0900

    업버튼 수정
    수정1

--- js/topbutton.js
+++ js/topbutton.js
@@ -1,4 +1,14 @@
-const $topBtn = document.querySelector(".moveTopBtn");
-        $topBtn.onclick = () => {
-            window.scrollTo({ top: 0, behavior: "smooth" });  
-        }
+const $topBtns = document.querySelectorAll(".moveTopBtn");
+
+$topBtns.forEach(($btn) => {
+    $btn.onclick = () => {
+        // 스크롤 애니메이션 시작
+        window.scrollTo({ top: 0, behavior: "smooth" });
+
+        // 스크롤 애니메이션이 끝난 후 원래 상태로 돌아오도록 설정
+        setTimeout(() => {
+            $btn.querySelector('.default-img').style.display = 'block'; // 기본 이미지 보이기
+            $btn.querySelector('.hover-img').style.display = 'none'; // 호버 이미지 숨기기
+        }, 500); // 스크롤 애니메이션 시간과 일치하도록 설정 (0.5초)
+    };
+});
