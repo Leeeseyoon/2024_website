f6cf001fb6e3da0b7f07020deea29c8ba906b326
seongwoo <163240206+Yangseongwoo@users.noreply.github.com>
2024-11-12 12:45:55 +0900

    혹시 몰라 data 추가

---
+++ js/data.js
@@ -0,0 +1,17 @@
+export const projectData = [
+    {
+        id: 1,
+        title: "ANAKNE(굿즈디자인)",
+        author: "강찬우",
+        imagePath: "images/img1.jpg",
+        fullTitle: "ANAKNE(굿즈디자인)_강찬우"
+    },
+    {
+        id: 2,
+        title: "제주여행",
+        author: "김승빈",
+        imagePath: "images/img2.jpg",
+        fullTitle: "제주여행_김승빈"
+    },
+    // ... 나머지 프로젝트
+];
